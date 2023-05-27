c51c1478c5b797a2289c2e4ca92d3cb703c9428b
Andrew <globalshow6@gmail.com>
2023-05-27 13:40:53 +0300

    React Hooks Tutorial: 17 - useContext Hook Part 3

--- src/components/ComponentE.js
+++ src/components/ComponentE.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import ComponentF from "./ComponentF";
+import {ChannelContext, UserContext} from "../App";
 
 function ComponentE() {
+	const user = useContext(UserContext)
+	const chanel = useContext(ChannelContext)
+
 	return (
 		<div>
+			{user} - {chanel}
 			<ComponentF />
 		</div>
 	);
