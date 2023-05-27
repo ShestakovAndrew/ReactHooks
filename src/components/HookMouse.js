13ee6f2822f6b1643e20084bfec917d57b494430
Andrew <globalshow6@gmail.com>
2023-05-27 12:03:13 +0300

    React Hooks Tutorial: 9 - Run effects only once

---
+++ src/components/HookMouse.js
@@ -0,0 +1,25 @@
+import React, {useState, useEffect} from 'react'
+
+function HookMouse() {
+	const [x, setX] = useState(0)
+	const [y, setY] = useState(0)
+
+	const logMousePosition = e => {
+		console.log("Вызвался logMousePosition")
+		setX(e.clientX)
+		setY(e.clientY)
+	}
+
+	useEffect(() => {
+		console.log("Вызвался useEffect")
+		window.addEventListener('mousemove', logMousePosition)
+	}, [])
+
+	return (
+		<div>
+			X - {x} Y - {y}
+		</div>
+	);
+}
+
+export default HookMouse;
