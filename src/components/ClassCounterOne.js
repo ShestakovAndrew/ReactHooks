c5a2175aefd800bb4b0637a2b30709b9f0953462
Andrew <globalshow6@gmail.com>
2023-05-27 11:48:38 +0300

    React Hooks Tutorial: 8 - Conditionally run effects

--- src/components/ClassCounterOne.js
+++ src/components/ClassCounterOne.js
@@ -4,7 +4,8 @@ class ClassCounterOne extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			count: 0
+			count: 0,
+			name: ''
 		}
 	}
 
@@ -12,13 +13,20 @@ class ClassCounterOne extends Component {
 		document.title = `Нажат ${this.state.count} раз`
 	}
 
-	componentDidUpdate() {
-		document.title = `Нажат ${this.state.count} раз`
+	componentDidUpdate(prevProps, prevState) {
+		if (prevState.count !== this.state.count)
+		{
+			console.log('Обновление документа')
+			document.title = `Нажат ${this.state.count} раз`
+		}
 	}
 
 	render() {
 		return (
 			<div>
+				<input type='text' value={this.state.name} onChange={event => {
+					this.setState({name: event.target.value})
+				}}/>
 				<button onClick={() => this.setState({ count: this.state.count + 1})}>
 					Нажал {this.state.count} раз
 				</button>
--- src/components/HookCounterOne.js
+++ src/components/HookCounterOne.js
@@ -2,13 +2,16 @@ import React, {useState, useEffect} from 'react'
 
 function HookCounterOne() {
 	const [count, setCount] = useState(0)
+	const [name, setName] = useState('')
 
 	useEffect(() => {
+		console.log('useEffect - обновился')
 		document.title = `Ты нажал ${count} раз`
-	})
+	}, [count])
 
 	return (
 		<div>
+			<input type='text' value={name} onChange={event => setName(event.target.value)}/>
 			<button onClick={() => setCount(count + 1)}>
 				Нажал {count} раз
 			</button>
