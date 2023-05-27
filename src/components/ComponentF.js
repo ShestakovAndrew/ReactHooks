b234d6fca93c1a2e20cc456b2a81a22f057082ad
Andrew <globalshow6@gmail.com>
2023-05-27 13:32:28 +0300

    React Hooks Tutorial: 16 - useContext Hook Part 2

---
+++ src/components/ComponentF.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {UserContext, ChannelContext} from "../App";
+
+function ComponentF() {
+	return (
+		<UserContext.Consumer>
+			{user => {
+				return (
+					<ChannelContext.Consumer>
+						{channel => {
+							return (
+								<div>
+									User context value {user}, {channel}
+								</div>
+							)
+						}}
+					</ChannelContext.Consumer>
+				)
+			}}
+		</UserContext.Consumer>
+	)
+}
+
+export default ComponentF
